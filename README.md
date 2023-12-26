6b0f14824c37384403f32ce8fc25cfd1149d0452
Mateus <mateusfaustino2022@gmail.com>
2023-12-26 13:52:48 -0300

    dockerfile

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # docker_php_apache_mysql
+
+## step 1: build image
+
+```
+    docker build -t my_php_image 
+```
+
+## step 2: run container
+
+```
+    docker run -i --rm -v ${PWD}:/var/www/html --name my_php_container -p 3000:80 my_php_image
+```
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>
+        olá mundo!
+    </h1>
+    <h2>
+        <?php
+           echo 'PHP Version: '.phpversion();
+        ?>
+    </h2>
+</body>
+</html>
